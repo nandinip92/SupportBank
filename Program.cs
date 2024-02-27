df793605097270839c784f4326ec0bd34be51378
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-27 15:09:39 +0000

    feat:Adding service

--- Program.cs
+++ Program.cs
@@ -1,29 +1,16 @@
-﻿using SupportBank.SupportBankManagement;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using SupportBank.SupportBankManagement;
 
-var fileName = "./data/Transactions2014.csv";
+var serviceProvider = new ServiceCollection()
+    .AddTransient<SupportBankApp>()
+    .AddLogging(loggingBuilder =>
+    {
+        loggingBuilder.ClearProviders();
+        loggingBuilder.AddNLog();
+    })
+    .BuildServiceProvider();
 
-var Bank = new Bank();
-Bank.ReadTransactionsFile(fileName);
-
-Console.WriteLine("Helooo....!!!");
-Console.WriteLine("Please select from the following options");
-Console.WriteLine("[1] Get All the Records");
-Console.WriteLine("[2] Get Transactions of a specific person");
-var option = int.Parse(Console.ReadLine() ?? "0");
-
-switch (option)
-{
-    case 1:
-        Bank.GetAllRecords();
-        break;
-    case 2:
-        Console.WriteLine(
-            "Please Enter the person name you want know the transaction details >>> "
-        );
-        var name = Console.ReadLine() ?? "";
-        Bank.GetAllTransactionsOfAccount(name.ToUpper());
-        break;
-    default:
-        Console.WriteLine("Invalid option, Please Enter a valid option...!!!");
-        break;
-}
+var supportBankApp = serviceProvider.GetRequiredService<SupportBankApp>();
+supportBankApp.StartService();
