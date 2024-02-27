e2e12f60c79c885f48e47c1883675456c1d1891f
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-27 15:08:18 +0000

    feat: Adding user interface

---
+++ SupportBankManagement/UserInterface.cs
@@ -0,0 +1,36 @@
+namespace SupportBank.SupportBankManagement;
+class UserInterface
+{
+    void RunUI()
+    {
+        //var fileName = "./data/Transactions2014.csv";
+        var fileName = "./data/DodgyTransactions2015.csv";
+
+        var Bank = new Bank();
+        Bank.ReadTransactionsFile(fileName);
+
+        Console.WriteLine("Helooo Welcome....!!!");
+        Console.WriteLine("[1] Get All the Records");
+        Console.WriteLine("[2] Get Transactions of a specific person");
+        Console.Write("Please select an option from the above >>>");
+
+        var option = int.Parse(Console.ReadLine() ?? "0");
+
+        switch (option)
+        {
+            case 1:
+                Bank.GetAllRecords();
+                break;
+            case 2:
+                Console.Write(
+                    "Please Enter the person name you want know the transaction details >>> "
+                );
+                var name = Console.ReadLine() ?? "";
+                Bank.GetAllTransactionsOfAccount(name.ToUpper());
+                break;
+            default:
+                Console.WriteLine("Invalid option, Please Enter a valid option...!!!");
+                break;
+        }
+    }
+}
